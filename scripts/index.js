fb5e42e6bf32d6dc7b604db0326b2c4efeaf8549
HonggangBu <honggang.bu@ndsu.edu>
2022-02-21 13:59:03 -0600

    wheat calculator done
    wheat calculator coding is done
    but need to update the Medium productivity and High productivity conventional data
    now wheat and sunflower calculators are done, only corn calculator left

--- scripts/index.js
+++ scripts/index.js
@@ -18,7 +18,7 @@ $(function () {
 
 
     //wheat & durum functions//
-    AddOptions($("#wheatPriceSelect"), 3, 1, 20, 0); // auto add wheat price list
+    AddOptions($("#wheatPriceSelect"), 3, 1, 15, 0); // auto add wheat price list
     AddOptions($("#wheatNitrogenPriceSelect"), 0.2, 0.1, 2.0, 1); // auto add wheat nitrogen cost list
     OnWheatCalculateBtnClicked(); // on wheat Calculate Btn Clicked, display result
 
@@ -50,7 +50,7 @@ function GetSoilTestNitrateCredit(inputControlId) {
 // return previous crop nitrogen credit value
 // parameter "SelectGroupId" is the ID of any select group control
 function GetPreviousCropNitrogenCredit(SelectGroupId) {
-    var gc = "#" + SelectGroupId;
+    let gc = "#" + SelectGroupId;
     //var selectedIndex = $(gc).prop("selectedIndex");
     return $(gc).val();
 }
@@ -58,10 +58,10 @@ function GetPreviousCropNitrogenCredit(SelectGroupId) {
 // calculate and return soil organic matter nitrogen credit
 // parameter "inputControlId" is the ID of a specific percentage organic matter input value
 function GetOrganicMatterCredit(inputControlId) {
-    var inputValue = $("#" + inputControlId).val();
-    var percentageThreshold = 5;
-    var coef = 50.0;
-    var credit = 0;
+    let inputValue = $("#" + inputControlId).val();
+    const percentageThreshold = 5;
+    const coef = 50.0;
+    let credit = 0;
     if (inputValue > percentageThreshold)
         credit = (inputValue - percentageThreshold) * coef;
     return credit;
@@ -69,17 +69,17 @@ function GetOrganicMatterCredit(inputControlId) {
 
 // get base value from a matrix or table
 function GetBaseValue(baseTable, cropPriceSelectControlId, nitrogenPriceSelectControlId) {
-    var baseValue;
-    var cropPriceIndex = parseInt($("#" + cropPriceSelectControlId).prop("selectedIndex"));
-    var nitrogenPriceIndex = parseInt($("#" + nitrogenPriceSelectControlId).prop("selectedIndex"));
+    let baseValue;
+    let cropPriceIndex = parseInt($("#" + cropPriceSelectControlId).prop("selectedIndex"));
+    let nitrogenPriceIndex = parseInt($("#" + nitrogenPriceSelectControlId).prop("selectedIndex"));
     baseValue = baseTable[cropPriceIndex][nitrogenPriceIndex];
     return baseValue;
 }
 
 // General calculation model
 // for sunflower, the tillageCredit is always equal to 0 because the base table already contains tillage credit
-function GetFinalResult(baseValue, soilTestNitrateCredit, organicMatterCredit, prevCropCredit, tillageCredit) {
-    var v = Math.round(baseValue - soilTestNitrateCredit - organicMatterCredit - prevCropCredit - tillageCredit);
+function GetFinalResult(baseValue, soilTestNitrateCredit, organicMatterCredit, prevCropCredit) {
+    let v = Math.round(baseValue - soilTestNitrateCredit - organicMatterCredit - prevCropCredit);
     return v > 0 ? v : 0;
 }
 
@@ -108,18 +108,21 @@ function GetNewTable(existingTable, difference) {
 
 // calculate the rest sunflower base table values based on existing table values
 function GetSunflowerNewDataTables() {
-    sunflowerWesternOilseedConventionaltill = GetNewTable(sunflowerWesternOilseedLongtermnotill, 50);
-    sunflowerWesternConfectionConventionaltill = GetNewTable(sunflowerWesternConfectionLongtermnotill, 50);
-    sunflowerWesternConfectionMinimalnotill = GetNewTable(sunflowerWesternConfectionConventionaltill, 20);
-    sunflowerWesternOilseedMinimalnotill = GetNewTable(sunflowerWesternOilseedConventionaltill, 20);
-    sunflowerEasternConfectionMinimalnotill = GetNewTable(sunflowerWesternConfectionConventionaltill, 20);
-    sunflowerEasternOilseedMinimalnotill = GetNewTable(sunflowerEasternOilseedConventionaltill, 20);
-    sunflowerLangdonConfectionConventionaltill = GetNewTable(sunflowerEasternConfectionConventionaltill, -40);
-    sunflowerLangdonOilseedConventionaltill = GetNewTable(sunflowerEasternOilseedConventionaltill, -40);
-    sunflowerLangdonConfectionLongtermnotill = GetNewTable(sunflowerEasternConfectionLongtermnotill, -40);
-    sunflowerLangdonOilseedLongtermnotill = GetNewTable(sunflowerEasternOilseedLongtermnotill, -40);
-    sunflowerLangdonConfectionMinimalnotill = GetNewTable(sunflowerEasternConfectionMinimalnotill, -40);
-    sunflowerLangdonOilseedMinimalnotill = GetNewTable(sunflowerEasternOilseedMinimalnotill, -40);
+    const sfConvTillDiff = 50;
+    const sfMinNotillDiff = 20;
+    const sfLangdonDiff = -40;
+    sunflowerWesternOilseedConventionaltill = GetNewTable(sunflowerWesternOilseedLongtermnotill, sfConvTillDiff);
+    sunflowerWesternConfectionConventionaltill = GetNewTable(sunflowerWesternConfectionLongtermnotill, sfConvTillDiff);
+    sunflowerWesternConfectionMinimalnotill = GetNewTable(sunflowerWesternConfectionConventionaltill, sfMinNotillDiff);
+    sunflowerWesternOilseedMinimalnotill = GetNewTable(sunflowerWesternOilseedConventionaltill, sfMinNotillDiff);
+    sunflowerEasternConfectionMinimalnotill = GetNewTable(sunflowerWesternConfectionConventionaltill, sfMinNotillDiff);
+    sunflowerEasternOilseedMinimalnotill = GetNewTable(sunflowerEasternOilseedConventionaltill, sfMinNotillDiff);
+    sunflowerLangdonConfectionConventionaltill = GetNewTable(sunflowerEasternConfectionConventionaltill, sfLangdonDiff);
+    sunflowerLangdonOilseedConventionaltill = GetNewTable(sunflowerEasternOilseedConventionaltill, sfLangdonDiff);
+    sunflowerLangdonConfectionLongtermnotill = GetNewTable(sunflowerEasternConfectionLongtermnotill, sfLangdonDiff);
+    sunflowerLangdonOilseedLongtermnotill = GetNewTable(sunflowerEasternOilseedLongtermnotill, sfLangdonDiff);
+    sunflowerLangdonConfectionMinimalnotill = GetNewTable(sunflowerEasternConfectionMinimalnotill, sfLangdonDiff);
+    sunflowerLangdonOilseedMinimalnotill = GetNewTable(sunflowerEasternOilseedMinimalnotill, sfLangdonDiff);
 }
 
 // get the string of the combination of sunflower region, sunflower type, and tillage type based on user selection
@@ -133,7 +136,7 @@ function GetSunflowerRegionTillageSeedSelectionCombination() {
 
 // get the sunflower base table (N recommendation table before credits) per the user selection
 function GetSunflowerValueTable(userSelection) {
-    var tb;
+    let tb;
     switch (userSelection) {
         case "west_confection_longNoTill":
             //tb = sunflowerWesternConfectionLongtermnotill.slice();
@@ -208,7 +211,7 @@ function GetSunflowerValueTable(userSelection) {
             tb = JSON.parse(JSON.stringify(sunflowerLangdonOilseedMinimalnotill));
             break;
         default:
-            alert("No valid selection!");
+            alert("Something is wrong!");
     }
     return tb;
 }
@@ -219,7 +222,7 @@ function OnSunflowerCalculateBtnClicked() {
         let userSelectionStr = GetSunflowerRegionTillageSeedSelectionCombination();
         let baseValueTable = GetSunflowerValueTable(userSelectionStr);
         let finalResult = GetFinalResult(GetBaseValue(baseValueTable, "sfPriceSelect", "sfNitrogenPriceSelect"), GetSoilTestNitrateCredit("sfSoilTestNitrateInput"),
-            GetOrganicMatterCredit("sfOrganicMatterInput"), GetPreviousCropNitrogenCredit("sfPreviousCropSelect"), 0);
+            GetOrganicMatterCredit("sfOrganicMatterInput"), GetPreviousCropNitrogenCredit("sfPreviousCropSelect"));
 
         $("#sfResultText").text(finalResult);
     });
@@ -281,10 +284,125 @@ function OnCornCalculateBtnClicked() {
 /////////////////////////////////////////////
 
 /////////////////////  WHEAT FUNCTIONS  ////////////////////////
+
+// 
+// get the string of the combination of wheat region, historical productivity, and tillage type based on user selection
+function GetWheatRegionTillageProductivitySelectionCombination() {
+    let selections = "";
+    selections = $("input[name='wheatRegion']:checked").val() + "_"
+        + $("input[name='wheatProductivity']:checked").val() + "_"
+        + $("input[name='wheatTillage']:checked").val();
+    return selections;
+}
+
+// This function return the actual and exact base value based on user selections
+// The returned value equals base value from one of the nine base tables plus tillage credit
+// That means tillage credit is already included in the returned value
+function GetWheatBaseValue(userSelection) {
+    const minNotillDiff = 20;
+    const eastWestLongNotillDiff = -50;
+    const langdonLongNotillDiff = -30;
+    let v = 0;
+
+    switch (userSelection) {
+        case "east_low_convTill":
+            v = GetBaseValue(wheatEastLowConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect");
+            break;
+        case "east_low_minNoTill":
+            v = GetBaseValue(wheatEastLowConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + minNotillDiff;
+            break;
+        case "east_low_longNoTill":
+            v = GetBaseValue(wheatEastLowConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + eastWestLongNotillDiff;
+            break;
+        case "west_low_convTill":
+            v = GetBaseValue(wheatWestLowConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect");
+            break;
+        case "west_low_minNoTill":
+            v = GetBaseValue(wheatWestLowConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + minNotillDiff;
+            break;
+        case "west_low_longNoTill":
+            v = GetBaseValue(wheatWestLowConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + eastWestLongNotillDiff;
+            break;
+        case "langdon_low_convTill":
+            v = GetBaseValue(wheatLangdonLowConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect");
+            break;
+        case "langdon_low_minNoTill":
+            v = GetBaseValue(wheatLangdonLowConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + minNotillDiff;
+            break;
+        case "langdon_low_longNoTill":
+            v = GetBaseValue(wheatLangdonLowConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + langdonLongNotillDiff;
+            break;
+
+        case "east_medium_convTill":
+            v = GetBaseValue(wheatEastMediumConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect");
+            break;
+        case "east_medium_minNoTill":
+            v = GetBaseValue(wheatEastMediumConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + minNotillDiff;
+            break;
+        case "east_medium_longNoTill":
+            v = GetBaseValue(wheatEastMediumConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + eastWestLongNotillDiff;
+            break;
+        case "west_medium_convTill":
+            v = GetBaseValue(wheatWestMediumConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect");
+            break;
+        case "west_medium_minNoTill":
+            v = GetBaseValue(wheatWestMediumConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + minNotillDiff;
+            break;
+        case "west_medium_longNoTill":
+            v = GetBaseValue(wheatWestMediumConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + eastWestLongNotillDiff;
+            break;
+        case "langdon_medium_convTill":
+            v = GetBaseValue(wheatLangdonMediumConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect");
+            break;
+        case "langdon_medium_minNoTill":
+            v = GetBaseValue(wheatLangdonMediumConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + minNotillDiff;
+            break;
+        case "langdon_medium_longNoTill":
+            v = GetBaseValue(wheatLangdonMediumConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + langdonLongNotillDiff;
+            break;
+
+        case "east_high_convTill":
+            v = GetBaseValue(wheatEastHighConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect");
+            break;
+        case "east_high_minNoTill":
+            v = GetBaseValue(wheatEastHighConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + minNotillDiff;
+            break;
+        case "east_high_longNoTill":
+            v = GetBaseValue(wheatEastHighConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + eastWestLongNotillDiff;
+            break;
+        case "west_high_convTill":
+            v = GetBaseValue(wheatWestHighConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect");
+            break;
+        case "west_high_minNoTill":
+            v = GetBaseValue(wheatWestHighConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + minNotillDiff;
+            break;
+        case "west_high_longNoTill":
+            v = GetBaseValue(wheatWestHighConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + eastWestLongNotillDiff;
+            break;
+        case "langdon_high_convTill":
+            v = GetBaseValue(wheatLangdonHighConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect");
+            break;
+        case "langdon_high_minNoTill":
+            v = GetBaseValue(wheatLangdonHighConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + minNotillDiff;
+            break;
+        case "langdon_high_longNoTill":
+            v = GetBaseValue(wheatLangdonHighConventionaltill, "wheatPriceSelect", "wheatNitrogenPriceSelect") + langdonLongNotillDiff;
+            break;
+        default:
+            alert("Something is wrong!");
+    }
+    return v;
+}
+
+
 // 
 function OnWheatCalculateBtnClicked() {
     $("#wheatCalculateBtn").click(function () {
-        $("#wheatResultText").text("The calculate button was clicked.");
+        let userSelectionStr = GetWheatRegionTillageProductivitySelectionCombination();
+        let baseValue = GetWheatBaseValue(userSelectionStr);
+        let finalResult = GetFinalResult(baseValue, GetSoilTestNitrateCredit("wheatSoilTestNitrateInput"),
+            GetOrganicMatterCredit("wheatOrganicMatterInput"), GetPreviousCropNitrogenCredit("wheatPreviousCropSelect"));
+        $("#wheatResultText").text(finalResult);
     });
 }
 
